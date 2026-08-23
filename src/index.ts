fadb7c7fbf45af6e58d0a7d6f655ec6b80d61b7c
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-23 19:51:34 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as ClaudiaComments } from './ClaudiaComments';
+export type { ClaudiaCommentsProps, ClaudiaCommentsCopy } from './ClaudiaComments';
+export type { ClaudiaComment } from './types';
